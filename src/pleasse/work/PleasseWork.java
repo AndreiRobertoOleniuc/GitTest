7a37f6c20c5170e24bf42f3d5ad9ba50cc2e8c0c
Andrei Oleniuc <andreipsn14@gmail.com>
2020-06-22 11:07:54 +0200

    please work

--- src/pleasse/work/PleasseWork.java
+++ src/pleasse/work/PleasseWork.java
@@ -15,7 +15,7 @@ public class PleasseWork {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hey this crap work YEEE");
     }
     
 }
